2f0fd2d0d395275c397b7986d1c0d38a651152f8
admin1 <admin1@ADMIN1-PC36>
2022-07-23 15:39:16 +0300

    Добавьте файлы проекта.

---
+++ Test/Test.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Fraction
+{
+private:
+    int integer = 0;
+    double numer;
+    double denom;
+public:
+    Fraction(int n = 0, int d = 1) {
+        this->numer = n;
+        this->denom = d;
+        this->integer = 0;
+    }
+    void comparison(Fraction a, Fraction b) {
+        a.reduce();
+        b.reduce();
+        if (double(a.numer) / double(a.denom) == double(b.numer) / double(b.denom) && a.integer == b.integer) {
+            cout << "Integer: " << a.integer << " " << a.numer << "/" << a.denom << " = " << "inteeger: " << b.integer << " " << b.numer << "/" << b.denom << endl;
+        }
+        if (a.integer > b.integer || double(a.numer) / double(a.denom) > double(b.numer) / double(b.denom)) {
+            cout << "Integer: " << a.integer << " " << a.numer << "/" << a.denom << " > " << "inteeger: " << b.integer << " " << b.numer << "/" << b.denom << endl;
+        }
+        if (a.integer < b.integer || double(a.numer) / double(a.denom) < double(b.numer) / double(b.denom)) {
+            cout << "Integer: " << a.integer << " " << a.numer << "/" << a.denom << " < " << "inteeger: " << b.integer << " " << b.numer << "/" << b.denom << endl;
+        }
+    }
+    void reduce() {
+        while (this->numer >= this->denom) {
+            this->numer -= this->denom;
+            this->integer++;
+        }
+    }
+};
+int main()
+{
+    setlocale(LC_ALL, "Ukrainian");
+    double n1 = 0, d1 = 0, n2 = 0, d2 = 0;
+    cout << "Input first numer --> ";
+    cin >> n1;
+    cout << endl << "Input first denom --> ";
+    cin >> d1;
+    cout << endl <<  "Input second numer --> ";
+    cin >> n2;
+    cout << endl << "Input second denom --> ";
+    cin >> d2;
+    cout << endl;
+    Fraction object1(n1, d1), object2(n2, d2);
+    object1.comparison(object1, object2);
+}
